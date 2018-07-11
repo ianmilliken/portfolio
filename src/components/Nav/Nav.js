2abcbc11a190e3ff77e447628e110041cd48a6ed
Ian <ianmilliken@live.co.uk>
2018-07-11 15:35:51 +0100

    Added contact panel

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,25 +2,53 @@ import React from "react"
 import Link from "gatsby-link"
 import "./Nav.css"
 
+
 class Nav extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(e) {
+		this.props.onContactLink(e)
+	}
+
 	render() {
 		const { menu } = this.props
-		return (
-			<nav className="nav">
-				<div className="nav__link nav__link--l">
-					<span className="nav__line"></span>
-					<Link to="/contact/">
-						<span>Contact</span>
-					</Link>
-				</div>
-				<div className="nav__link nav__link--r">
-					<span className="nav__line"></span>
-					<Link to="/about/">
-						<span>About</span>
-					</Link>
-				</div>
-			</nav>
-		)
+		if (this.props.menu === 0) {
+			return (
+				<nav className="nav">
+					<div className="nav__link nav__link--l">
+						<span className="nav__line"></span>
+						<a href="/" onClick={this.handleClick}><span>Contact</span></a>
+					</div>
+					<div className="nav__link nav__link--r">
+						<span className="nav__line"></span>
+						<Link to="/about/">
+							<span>About</span>
+						</Link>
+					</div>
+				</nav>
+			)
+		} else {
+			return (
+				<nav className="nav">
+					<div className="nav__link nav__link--l">
+						<span className="nav__line"></span>
+						<Link to="/">
+							<span>Previous</span>
+						</Link>
+					</div>
+					<div className="nav__link nav__link--r">
+						<span className="nav__line"></span>
+						<Link to="/">
+							<span>Next</span>
+						</Link>
+					</div>
+				</nav>
+			)
+		}
 	}
 }
 
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -13,119 +13,121 @@ import Circle from "../components/Icons/Circle"
 
 export default class MainLayout extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      contactPanel: false
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			contact: false
+		}
+		this.handleContactLink = this.handleContactLink.bind(this)
+	}
 
-  componentDidMount() {
-    /*var circle_animation = anime({
-      targets: '.shape--left',
-      translateX: function() { return anime.random(-100, 25) + '%'; },
-      translateY: function() { return anime.random(-75, 150) + '%'; },
-      duration: function() { return anime.random(15000, 20000); },
-      direction: 'alternate',
-      loop: true,
-      easing: 'easeInCubic'
-    });
-    var triangle_animation = anime({
-      targets: '.shape--right',
-      translateX: function() { return anime.random(-75, 75) + '%'; },
-      translateY: function() { return anime.random(-50, 75) + '%'; },
-      duration: function() { return anime.random(10000, 15000); },
-      direction: 'alternate',
-      loop: true,
-      easing: 'easeInCubic'
-    });*/
-  }
+	componentDidMount() {
+	/*var circle_animation = anime({
+	  targets: '.shape--left',
+	  translateX: function() { return anime.random(-100, 25) + '%'; },
+	  translateY: function() { return anime.random(-75, 150) + '%'; },
+	  duration: function() { return anime.random(15000, 20000); },
+	  direction: 'alternate',
+	  loop: true,
+	  easing: 'easeInCubic'
+	});
+	var triangle_animation = anime({
+	  targets: '.shape--right',
+	  translateX: function() { return anime.random(-75, 75) + '%'; },
+	  translateY: function() { return anime.random(-50, 75) + '%'; },
+	  duration: function() { return anime.random(10000, 15000); },
+	  direction: 'alternate',
+	  loop: true,
+	  easing: 'easeInCubic'
+	});*/
+	}
 
-  getLocalTitle() {
-    function capitalize(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    const pathPrefix = config.pathPrefix ? config.pathPrefix : "/";
-    const currentPath = this.props.location.pathname
-      .replace(pathPrefix, "")
-      .replace("/", "");
-    let title = "";
-    if (currentPath === "") {
-      title = "Home";
-    } else if (currentPath === "tags/") {
-      title = "Tags";
-    } else if (currentPath === "categories/") {
-      title = "Categories";
-    } else if (currentPath === "about/") {
-      title = "About";
-    } else if (currentPath === "work/") {
-      title = "Work";
-    } else if (currentPath.indexOf("posts")) {
-      title = "Article";
-    } else if (currentPath.indexOf("tags/")) {
-      const tag = currentPath
-        .replace("tags/", "")
-        .replace("/", "")
-        .replace("-", " ");
-      title = `Tagged in ${capitalize(tag)}`;
-    } else if (currentPath.indexOf("categories/")) {
-      const category = currentPath
-        .replace("categories/", "")
-        .replace("/", "")
-        .replace("-", " ");
-      title = `${capitalize(category)}`;
-    }
-    return title;
-  }
+	handleContactLink(e) {
+		e.preventDefault()
+		const { contact} = this.state
+		this.setState({
+			contact: contact ? false : true
+		})
+	}
 
-  getLocalMenu() {
-    function capitalize(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    const pathPrefix = config.pathPrefix ? config.pathPrefix : "/";
-    const currentPath = this.props.location.pathname
-      .replace(pathPrefix, "")
-      .replace("/", "");
-    let menu = 0;
-    if (currentPath === "work/") {
-      menu = 1;
-    }
-    return menu;
-  }
+	getLocalTitle() {
+		function capitalize(string) {
+			return string.charAt(0).toUpperCase() + string.slice(1)
+		}
+		const pathPrefix = config.pathPrefix ? config.pathPrefix : "/"
+		const currentPath = this.props.location.pathname.replace(pathPrefix, "").replace("/", "")
+		let title = ""
+		if (currentPath === "") {
+			title = "Home"
+		} else if (currentPath === "tags/") {
+			title = "Tags"
+		} else if (currentPath === "categories/") {
+			title = "Categories"
+		} else if (currentPath === "about/") {
+			title = "About"
+		} else if (currentPath === "work/") {
+			title = "Work"
+		} else if (currentPath.indexOf("posts")) {
+			title = "Article"
+		} else if (currentPath.indexOf("tags/")) {
+			const tag = currentPath.replace("tags/", "").replace("/", "").replace("-", " ")
+			title = `Tagged in ${capitalize(tag)}`
+		} else if (currentPath.indexOf("categories/")) {
+			const category = currentPath.replace("categories/", "").replace("/", "").replace("-", " ")
+			title = `${capitalize(category)}`
+		}
+		return title
+	}
 
-  render() {
-    const { children } = this.props;
-    const pathname = this.props.location.pathname
-    console.log(pathname)
-    let background_color
-    let text_color
-    let location_class
-    if (pathname.includes("/work/")) {
-      background_color = "#F9F9F9"
-      text_color = "#111310"
-      location_class = "work"
-    } else if (pathname === "/about/") {
-      background_color = "#00DEA1"
-      text_color = "#FFFFFF"
-      location_class = "about"
-    } else {
-      background_color = "#111310"
-      text_color = "#FFFFFF"
-      location_class = "index"
-    }
-    return (
-      <div className={location_class} style={{ backgroundColor: background_color, color: text_color }}>
-        <Helmet>
-          <title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
-          <meta name="description" content={config.siteDescription} />
-        </Helmet>
-        <Circle classes="shape shape--left" color="#00DEA1"/>
-        <Triangle classes="shape shape--right" color="#00DEA1"/>
-        <Header config={config} />
-        <Nav menu={this.getLocalMenu()} />
-        <main>{children()}</main>
-        <Footer config={config} />
-      </div>
-    );
-  }
+	getLocalMenu() {
+		function capitalize(string) {
+			return string.charAt(0).toUpperCase() + string.slice(1)
+		}
+		const pathPrefix = config.pathPrefix ? config.pathPrefix : "/"
+		const currentPath = this.props.location.pathname.replace(pathPrefix, "").replace("/", "")
+		let menu = 0;
+		if (currentPath === "work/") {
+			menu = 1
+		}
+		return menu
+	}
+
+  	render() {
+		const { children } = this.props
+		const pathname = this.props.location.pathname
+		let background_color
+		let text_color
+		let location_class
+		if (pathname.includes("/work/")) {
+			background_color = "#F9F9F9"
+			text_color = "#111310"
+			location_class = "work"
+		} else if (pathname === "/about/") {
+			background_color = "#00DEA1"
+			text_color = "#FFFFFF"
+			location_class = "about"
+		} else {
+			background_color = "#111310"
+			text_color = "#FFFFFF"
+			location_class = "index"
+		}
+		return (
+			<div className={location_class} style={{ backgroundColor: background_color, color: text_color }}>
+				<Helmet>
+					<title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
+					<meta name="description" content={config.siteDescription} />
+				</Helmet>
+				<Circle classes="shape shape--left" color="#00DEA1"/>
+				<Triangle classes="shape shape--right" color="#00DEA1"/>
+				<Header config={config} />
+				<Nav menu={this.getLocalMenu()} onContactLink={this.handleContactLink} />
+				<main>{children()}</main>
+				<Footer config={config} />
+				<div className={`overlay ${ this.state.contact ? `is-active` : ``}`}></div>
+				<div className={`contact-panel ${ this.state.contact ? `is-active` : ``}`}>
+					<h2>Get in touch</h2>
+				</div>
+	  		</div>
+		)
+	}
 }
